d85d4b9b4876dc04a5d5c781fcde5863131bd981
uberfastman <wrenjr@verizon.net>
2015-10-07 01:05:27 -0400

    final test before live deployment on /r/knife_swap

--- knifeswap-raffle-bot.py
+++ knifeswap-raffle-bot.py
@@ -11,15 +11,16 @@ import sys
 r = praw.Reddit(user_agent="knifeswap_raffle_automator 1.0 by /u/uberfastman")
 r.login(os.environ["REDDIT_USER"], os.environ["REDDIT_PASS"])
 
-# subreddit = r.get_subreddit("knife_swap")
-subreddit = r.get_subreddit("bot_testing_ground")
+subreddit = r.get_subreddit("knife_swap")
+# subreddit = r.get_subreddit("bot_testing_ground")
 
 python_mod_list = [str(mod) for mod in r.get_moderators(subreddit)]
 
-logging.basicConfig(filename="raffle_bot_log.txt", level=logging.INFO)
+# logging.basicConfig(filename="raffle_bot_log.txt", level=logging.INFO)
 
-with open("saved_parsed_comments.txt", "r") as parse_history:
-    already_parsed_comments = parse_history.read().splitlines()
+already_parsed_comments = []
+# with open("saved_parsed_comments.txt", "r") as parse_history:
+#     already_parsed_comments = parse_history.read().splitlines()
 
 
 def parse_to_integer(string):
@@ -96,58 +97,56 @@ while True:
                                 winner = random.randint(0, total_slots)
 
                                 comment_msg = "Thank you for hosting a raffle on /r/%s with %d slots.\n\nThe winner of the raffle is the redditor who chose raffle slot number %d!\n\n\n&nbsp;\n\n\n[^^Contact ^^Creator](https://www.reddit.com/message/compose/?to=uberfastman) ^^| [^^Source ^^Code](https://github.com/uberfastman/knifeswap-raffle-bot)" % (str(subreddit), total_slots, winner)
-                                logging.info("Raffle was drawn for comment at %s" % comment_link)
+
+                                # logging.info("Raffle was drawn for comment at %s" % comment_link)
+                                print "Raffle was drawn for comment at %s" % comment_link
 
                             else:
                                 comment_msg = "No number of slots for the raffle was specified. Please try again.\n\n\n&nbsp;\n\n\n[^^Contact ^^Creator](https://www.reddit.com/message/compose/?to=uberfastman) ^^| [^^Source ^^Code](https://github.com/uberfastman/knifeswap-raffle-bot)"
 
-                                logging.info("Raffle was attempted but needed slot number for comment at %s" % comment_link)
+                                # logging.info("Raffle was attempted but needed slot number for comment at %s" % comment_link)
+                                print "Raffle was attempted but needed slot number for comment at %s" % comment_link
 
                             comment.reply(comment_msg)
 
                         # warns comment poster that they cannot call for a raffle drawing if they are not the original submission poster
                         else:
+                            comment_msg = "You are ***NOT*** the submitter of this raffle. You do ***NOT*** have permission to do the raffle drawing.\n\n\n&nbsp;\n\n\n[^^Contact ^^Creator](https://www.reddit.com/message/compose/?to=uberfastman) ^^| [^^Source ^^Code](https://github.com/uberfastman/knifeswap-raffle-bot)"
 
-                            if comment_author != "KNIFESWAP_RAFFLE_BOT":
-                                comment_msg = "You are ***NOT*** the submitter of this raffle. You do ***NOT*** have permission to do the raffle drawing.\n\n\n&nbsp;\n\n\n[^^Contact ^^Creator](https://www.reddit.com/message/compose/?to=uberfastman) ^^| [^^Source ^^Code](https://github.com/uberfastman/knifeswap-raffle-bot)"
+                            # logging.info("Raffle drawing call was made by incorrect user for comment %s" % comment_link)
+                            print "Raffle drawing call was made by incorrect user for comment %s" % comment_link
 
-                                logging.info("Raffle drawing call was made by incorrect user for comment %s" % comment_link)
+                            comment.reply(comment_msg)
 
-                                comment.reply(comment_msg)
-
-                    with open("saved_parsed_comments.txt", "a") as parse_history:
-                        parse_history.write(comment_id + "\n")
+                    # with open("saved_parsed_comments.txt", "a") as parse_history:
+                    #     parse_history.write(comment_id + "\n")
 
                     already_parsed_comments.append(comment_id)
 
         current_time = datetime.datetime.now() - datetime.timedelta(hours=4)
-        logging.info("The loop parsing new posts in /r/%s last executed at %s and took %s to run" % (str(subreddit), str(current_time), str(current_time - start_time)))
+
+        # logging.info("The loop parsing new posts in /r/%s last executed at %s and took %s to run" % (str(subreddit), str(current_time), str(current_time - start_time)))
         print "The loop parsing new posts in /r/%s last executed at %s and took %s to run" % (str(subreddit), str(current_time), str(current_time - start_time))
 
-    # catches any attribute errors and sends /u/uberfastman a private message with the error
-    except AttributeError as e:
-        private_message = "Your bot /u/KNIFESWAP_RAFFLE_B0T encountered the following error: %s." % e
-        # r.send_message("uberfastman", "KNIFESWAP RAFFLE BOT ERROR!", private_message)
-        logging.error("KNIFESWAP_RAFFLE_BOT encountered error: %s (line %s)" % (e, sys.exc_info()[-1].tb_lineno))
-        print "ERROR: %s (line %s)" % (e, sys.exc_info()[-1].tb_lineno)
-        pass
-
+    # catches any exceptions and sends /u/uberfastman a private message with the error
     except Exception as e:
-        private_message = "Your bot /u/KNIFESWAP_RAFFLE_B0T encountered the following error: %s." % e
-        # r.send_message("uberfastman", "KNIFESWAP RAFFLE BOT ERROR!", private_message)
-        logging.error("KNIFESWAP_RAFFLE_BOT encountered error: %s (line %s)" % (e, sys.exc_info()[-1].tb_lineno))
+        private_message = "ERROR: %s (line %s)" % (e, sys.exc_info()[-1].tb_lineno)
+        r.send_message("uberfastman", "KNIFESWAP RAFFLE BOT ERROR!", private_message)
+
+        # logging.error("KNIFESWAP_RAFFLE_BOT encountered error: %s (line %s)" % (e, sys.exc_info()[-1].tb_lineno))
         print "ERROR: %s (line %s)" % (e, sys.exc_info()[-1].tb_lineno)
         pass
 
-    # sleeps for 15 minutes before repeating the loop
     time_check = datetime.datetime.now() - datetime.timedelta(hours=4)
     midnight = time_check.replace(hour=0, minute=0, second=0, microsecond=0)
     time_diff = time_check - midnight
     sec_diff = time_diff.seconds
 
     if 27900 < sec_diff < 85500:
+        # logging.info("Starting daytime sleep at %s for 15 minutes." % str(time_check.time()))
         print "Starting daytime sleep at %s for 15 minutes." % str(time_check.time())
         # time.sleep(900)
     else:
+        # logging.info("Starting nighttime sleep at %s and will awaken again in 8 hours." % str(time_check.time()))
         print "Starting nighttime sleep at %s and will awaken again in 8 hours." % str(time_check.time())
         # time.sleep(28800)
